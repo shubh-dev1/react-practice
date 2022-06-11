50a65a5ea86ff3f7ff1199b0ffde6a51eb03df8c
Shubham <Singhalshubham610@gmail.com>
2022-06-11 23:08:24 +0530

    initialcommit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,117 @@ import ChangeText from './Components/ChangeText';
 import TemperaturController from './Components/TemperaturController';
 import SearchFilter from './Components/SearchFilter';
 import BasicForm from './Components/BasicForm';
+import Todo from './Components/Todo';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+import Calculator from './Components/Calculator';
+// import {IconContext} from 'react-icons'
+// import {FaReact} from 'react-icons/fa'
+// import { MdAlarm} from 'react-icons/md'
+
+
+
+
+// calculator
 function App() {
   return (
-    <div className="App">
-      <BasicForm/>
-        {/*<SearchFilter/>
-        {/* <TemperaturController/>
-     {/* <Counter/> */}
-     {/* <ChangeText/> */}
-    </div>
+   
+    <div className="todo-pp">
+<Calculator/>
+
+      </div>
+
+  
   );
 }
 
 export default App;
+
+
+
+
+
+// Todo app
+// function App() {
+//   return (
+   
+//     <div className="todo-pp">
+//   <TodoList/>
+
+//       </div>
+
+  
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <IconContext.Provider value={{color : 'blue' , size: '5rem'}}>
+//     <div className="App">
+
+//     <FaReact />
+//     <MdAlarm />
+//     </div>
+//     </IconContext.Provider>
+  
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//        {/*<BasicForm/>
+//         {/*<SearchFilter/>
+//         {/* <TemperaturController/>
+//      {/* <Counter/> */}
+//      {/* <ChangeText/> */}
+//     </div>
+//   );
+// }
+
+// export default App;
